ce3d91707829c9a720a863c64adb4ae4879505e0
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-10-06 23:09:28 -0600

    Sitio publico actualizado

---
+++ public/precache-manifest.50f9d5abd339c07a09940d68fd37ee97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44baefd155c769d0a4060b1da60aaeb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa146a10ffb7547bbd92",
+    "url": "/static/js/2.cfe06b73.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.cfe06b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01eae6b5713035a1912c",
+    "url": "/static/js/main.60e7eed1.chunk.js"
+  },
+  {
+    "revision": "f91229805f22e900e5d5",
+    "url": "/static/js/runtime-main.61151746.js"
+  }
+]);
